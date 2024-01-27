1b5124efe9b5a8550f94a666a20ef7f943203d5b
Franck <lemalindalin@gmail.com>
2024-01-27 08:23:30 -0500

    Initial Commit

---
+++ SwiftUiCharts/SwiftUiChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiChartsApp.swift
+//  SwiftUiCharts
+//
+//  Created by Franck Dalin Mbeutcha Tchuisseu on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
